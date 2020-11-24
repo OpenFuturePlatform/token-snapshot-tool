ec93116c5569e529a2cfc180a70382e3b723ba1c
trenatov <temirlan.renatov@gmail.com>
2020-11-24 17:28:19 +0600

    #8 Auto completion

--- snapshot.sh
+++ snapshot.sh
@@ -1,9 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # BUILD CHECK
 BUILD_JAR_PATH=./build/libs/snapshot-0.0.1-SNAPSHOT.jar
 if [ ! -f "$BUILD_JAR_PATH" ]; then
   ./gradlew build
+fi
+
+# AUTO COMPLETE
+if [ ! -f "snapshot_auto_complete.sh" ]; then
+  chmod +x snapshot.sh
+  _SNAPSHOT_COMPLETE=bash java -jar "$BUILD_JAR_PATH" >snapshot_auto_complete.sh
+  echo complete -F _snapshot ./snapshot.sh >>snapshot_auto_complete.sh
+  echo source "$PWD"/snapshot_auto_complete.sh >>~/.bashrc
+  echo source "$PWD"/snapshot_auto_complete.sh >>~/.bash_profile
 fi
 
 # RUN
