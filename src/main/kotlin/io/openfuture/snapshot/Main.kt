10d462fa766472d714a419a1f94481a0c034161a
trenatov <temirlan.renatov@gmail.com>
2020-11-27 15:38:06 +0600

    #6 from/to parameters should be optional

--- src/main/kotlin/io/openfuture/snapshot/Main.kt
+++ src/main/kotlin/io/openfuture/snapshot/Main.kt
@@ -9,15 +9,17 @@ import io.openfuture.snapshot.exporter.FileExporter
 import io.openfuture.snapshot.snapshotcreator.ArchivedNodeBasedSnapshotCreator
 import io.openfuture.snapshot.snapshotcreator.SnapshotCreator
 import io.openfuture.snapshot.snapshotcreator.TransferEventBasedSnapshotCreator
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.http.HttpService
 
 class CommandLineSnapshotRunner : CliktCommand(name = "snapshot") {
 
-    private val contractAddress: String by option(help = "Address of token Smart contract").required()
-    private val from: Int by option(help = "Start block number").int().required()
-    private val to: Int by option(help = "End block number").int().required()
-    private val nodeAddress: String by option(help = "Server url of node to connect").required()
-    private val filename: String by option(help = "Name of csv file to save").defaultLazy { "snapshot_at_block_$to.csv" }
-    private val mode: Mode by option(help = "Snapshot mode").convert { valueOf(it.toUpperCase()) }.default(EVENT)
+    private val contractAddress: String by option(help = "Address of token Smart contract", names = arrayOf("-c", "--contract")).required()
+    private val from: Int by option(help = "Start block number", names = arrayOf("-f", "--from")).int().default(0)
+    private val to: Int? by option(help = "End block number", names = arrayOf("-t", "--to")).int()
+    private val nodeAddress: String by option(help = "Server url of node to connect", names = arrayOf("-n", "--node-address")).required()
+    private val filename: String by option(help = "Name of csv file to save", names = arrayOf("-o", "--output")).defaultLazy { "snapshot_at_block_$to.csv" }
+    private val mode: Mode by option(help = "Snapshot mode", names = arrayOf("-m", "--mode")).convert { Mode.valueOf(it.toUpperCase()) }.default(EVENT)
 
     private val exporter: FileExporter = CsvSnapshotExporter()
 
@@ -27,13 +29,21 @@ class CommandLineSnapshotRunner : CliktCommand(name = "snapshot") {
             EVENT -> TransferEventBasedSnapshotCreator(nodeAddress)
         }
 
-        val results = creator.snapshot(contractAddress, from, to)
+        val toBlockNumber = to ?: Web3jHelper(nodeAddress).getLatestBlock()
+        val results = creator.snapshot(contractAddress, from, toBlockNumber)
         exporter.export(filename, results)
     }
 
     private enum class Mode {
         ARCHIVED,
         EVENT
+    }
+
+    class Web3jHelper(nodeAddress: String) {
+
+        private val web3j: Web3j = Web3j.build(HttpService(nodeAddress))
+
+        fun getLatestBlock(): Int = web3j.ethBlockNumber().send().blockNumber.toInt()
     }
 
 }
