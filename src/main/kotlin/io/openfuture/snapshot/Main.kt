54ce7af7f225849c035d3627594d7045562132c2
trenatov <temirlan.renatov@gmail.com>
2020-11-28 13:26:05 +0600

    #4 resolve conflicts

--- src/main/kotlin/io/openfuture/snapshot/Main.kt
+++ src/main/kotlin/io/openfuture/snapshot/Main.kt
@@ -6,9 +6,9 @@ import com.github.ajalt.clikt.parameters.types.int
 import io.openfuture.snapshot.CommandLineSnapshotRunner.Mode.*
 import io.openfuture.snapshot.exporter.CsvSnapshotExporter
 import io.openfuture.snapshot.exporter.FileExporter
-import io.openfuture.snapshot.snapshotcreator.ArchivedNodeBasedSnapshotCreator
-import io.openfuture.snapshot.snapshotcreator.SnapshotCreator
-import io.openfuture.snapshot.snapshotcreator.TransferEventBasedSnapshotCreator
+import io.openfuture.snapshot.snapshot.ArchivedNodeBasedSnapshotCreator
+import io.openfuture.snapshot.snapshot.SnapshotCreator
+import io.openfuture.snapshot.snapshot.TransferEventBasedSnapshotCreator
 
 class CommandLineSnapshotRunner : CliktCommand(name = "snapshot") {
 
